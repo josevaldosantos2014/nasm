9c404bd1d387a4cc99e2323d8ea1e99c9528a384
Josevaldo <josevaldo.santos2014@bol.com.br>
2024-09-05 14:30:43 +0000

    Criei um novo arquivo para versionamento

---
+++ ola.asm
@@ -0,0 +1,16 @@
+section .data
+    msg db 'Hello, World', 0
+
+section .text 
+    global _start
+
+_start:
+   mov rax,1        ; sys_white system call
+   mov rdi,1        ; file descriptor 1 (stdout)
+   mov rsi,msg      ; pointer to the message
+   mov rdx,13       ; message length
+   syscall
+
+   mov rax,60       ; sys_exit system call
+   xor rdi,rdi      ; exit code
+   syscall
